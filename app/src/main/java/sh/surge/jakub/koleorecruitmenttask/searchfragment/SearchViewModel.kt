5a4ffcb61812da9f57f7dd1b30f6b694d14b0a61
Kuba <jakub.rogala.95@gmail.com>
2020-10-06 17:37:19 +0200

    Calculate the distance

--- app/src/main/java/sh/surge/jakub/koleorecruitmenttask/searchfragment/SearchViewModel.kt
+++ app/src/main/java/sh/surge/jakub/koleorecruitmenttask/searchfragment/SearchViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModel
 import ext.launchIO
 import sh.surge.jakub.koleorecruitmenttask.data.station.Station
 import sh.surge.jakub.koleorecruitmenttask.repo.MainRepository
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 class SearchViewModel(private var repository: MainRepository) : ViewModel() {
 
@@ -15,4 +18,33 @@ class SearchViewModel(private var repository: MainRepository) : ViewModel() {
         return sharedStations
     }
 
+    fun getDistance(startStation: Station?, endStation: Station?): String {
+        var tempString = ""
+        if (startStation != null && endStation != null) {
+         val tempDistance = distance(startStation.latitude, startStation.longitude, endStation.latitude, endStation.longitude)
+            tempString = String.format("%.2f", tempDistance)
+        }
+        return "$tempString km"
+    }
+
+    private fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val theta = lon1 - lon2
+        var dist = (sin(deg2rad(lat1))
+                * sin(deg2rad(lat2))
+                + (cos(deg2rad(lat1))
+                * cos(deg2rad(lat2))
+                * cos(deg2rad(theta))))
+        dist = acos(dist)
+        dist = rad2deg(dist)
+        dist *= 60 * 1.1515
+        return dist
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return deg * Math.PI / 180.0
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return rad * 180.0 / Math.PI
+    }
 }
